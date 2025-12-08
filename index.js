7d11fa6ba21c947bb15211c3d4ca9a14ee56430e
tanvi183 <mneshat7@gmail.com>
2025-12-07 20:12:18 -0800

    added parcel delete api

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 3000;
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 // Middleware
 app.use(express.json());
@@ -50,6 +50,14 @@ async function run() {
       parcel.createdAt = new Date();
 
       const result = await parcelsCollection.insertOne(parcel);
+      res.send(result);
+    });
+
+    app.delete("/parcels/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+
+      const result = await parcelsCollection.deleteOne(query);
       res.send(result);
     });
 
