b2e457a2f705fdea71fe0a127085328a6e2ad9b3
tanvi183 <mneshat7@gmail.com>
2025-12-11 00:01:44 -0800

    updated rider work status after parcel delivered

--- index.js
+++ index.js
@@ -183,7 +183,8 @@ async function run() {
         query.riderEmail = riderEmail;
       }
       if (deliveryStatus) {
-        query.deliveryStatus = { $in: ["driver_assigned", "rider_arriving"] };
+        // query.deliveryStatus = {$in: ['driver_assigned', 'rider_arriving']}
+        query.deliveryStatus = { $nin: ["parcel_delivered"] };
       }
 
       const cursor = parcelsCollection.find(query);
@@ -193,13 +194,29 @@ async function run() {
 
     // parcle status accept and reject
     app.patch("/parcels/:id/status", async (req, res) => {
-      const { deliveryStatus } = req.body;
+      const { deliveryStatus, riderId } = req.body;
+
       const query = { _id: new ObjectId(req.params.id) };
       const updatedDoc = {
         $set: {
           deliveryStatus: deliveryStatus,
         },
       };
+
+      if (deliveryStatus === "parcel_delivered") {
+        // update rider information
+        const riderQuery = { _id: new ObjectId(riderId) };
+        const riderUpdatedDoc = {
+          $set: {
+            workStatus: "available",
+          },
+        };
+        const riderResult = await ridersCollection.updateOne(
+          riderQuery,
+          riderUpdatedDoc
+        );
+      }
+
       const result = await parcelsCollection.updateOne(query, updatedDoc);
       res.send(result);
     });
